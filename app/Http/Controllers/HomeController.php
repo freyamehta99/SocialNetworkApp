06b38fccd109da0f4358efce12df40cf1b098e30
freyamehta99 <freyamehta99@yahoo.com>
2018-05-23 12:14:27 +0530

    Second Commit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,7 +25,7 @@ class HomeController extends Controller
     // {
     //     return view('home');
     //     
-   // }
+       // }
      public function index()
     {
         $posts = Post::all();
